c49636b10096e63de63402ec9b798d1bb9043e97
zsofiacsaszar0 <zsofiacsaszar0@outlook.com>
2019-11-30 18:35:12 +0100

    Last commit

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -18,8 +18,8 @@ namespace UserMaintenance
         public Form1()
         {
             InitializeComponent();
-            label1.Text = Resource1.LastName; // label1
-            label2.Text = Resource1.FirstName; // label2
+            label1.Text = Resource1.FullName; // label1
+         
             button1.Text = Resource1.Add; // button1
 
             listBox1.DataSource = users;
@@ -31,8 +31,8 @@ namespace UserMaintenance
         {
             var u = new User()
             {
-                LastName = textBox1.Text,
-                FirstName = textBox2.Text
+                FullName = textBox1.Text,
+               
             };
             users.Add(u);
         }
